5fdaebe6e69b65192b995a4b0a7135d7eb9a5051
王洋 <wangyang1@u51.com>
2018-09-20 00:25:23 +0800

    update

--- src/main/java/com/github/wycm/TencentCrawler.java
+++ src/main/java/com/github/wycm/TencentCrawler.java
@@ -36,12 +36,9 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-/**
- * Created by wangyang on 2018/5/3.
- * 计算平移距离
- */
+
 public class TencentCrawler {
-    private static String BASE_PATH = "/Users/wangyang/Documents/";
+    private static String BASE_PATH = "";
     //小方块距离左边界距离
     private static int START_DISTANCE = 22;
     private static ChromeDriver driver = null;
@@ -55,13 +52,94 @@ public class TencentCrawler {
         crawl();
     }
 
+
+    public static void crawl(){
+        driver = new ChromeDriver();
+        for(int i = 0; i < 10; i++) {
+            try {
+                driver.manage().window().setSize(new Dimension(1024, 768));
+                driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+                driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+                driver.get("https://007.qq.com/online.html?ADTAG=capt.slide");
+                WebElement element = driver.findElement(By.cssSelector("a[data-type='1']"));
+                element.click();
+                Thread.sleep(2 * 1000);
+                driver.findElement(By.id("code")).click();
+                Actions actions = new Actions(driver);
+                driver.switchTo().frame("tcaptcha_popup");
+                String originalUrl = Jsoup.parse(driver.getPageSource()).select("[id=slideBkg]").first().attr("src");
+                System.out.println(originalUrl);
+                downloadOriginalImg(originalUrl, driver.manage().getCookies());
+                int distance = calcMoveDistance();
+                List<MoveEntity> list = getMoveEntity(distance);
+                element = driver.findElement(By.id("tcaptcha_drag_button"));
+                actions.clickAndHold(element).perform();
+                int d = 0;
+                for (MoveEntity moveEntity : list) {
+                    actions.moveByOffset(moveEntity.getX(), moveEntity.getY()).perform();
+                    System.out.println("向右总共移动了:" + (d = d + moveEntity.getX()));
+                    Thread.sleep(moveEntity.getSleepTime());
+                }
+                actions.release(element).perform();
+                Thread.sleep(2 * 1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+            driver.quit();
+    }
+    private static void downloadOriginalImg(String originalUrl, Set<Cookie> cookieSet) throws IOException {
+        CookieStore cookieStore = new BasicCookieStore();
+        cookieSet.forEach( c -> {
+            BasicClientCookie cookie = new BasicClientCookie(c.getName(), c.getValue());
+            cookie.setPath(c.getPath());
+            cookie.setDomain(c.getDomain());
+            cookie.setExpiryDate(c.getExpiry());
+            cookie.setSecure(true);
+            cookieStore.addCookie(cookie);
+        });
+        InputStream is = null;
+        try {
+            SSLContext sslContext = SSLContexts.custom()
+                    .loadTrustMaterial(KeyStore.getInstance(KeyStore.getDefaultType())
+                            , (chain, authType) -> true).build();
+            Registry<ConnectionSocketFactory> socketFactoryRegistry =
+                    RegistryBuilder.<ConnectionSocketFactory>create()
+                            .register("http", PlainConnectionSocketFactory.INSTANCE)
+                            .register("https", new SSLConnectionSocketFactory(sslContext))
+                            .build();
+            is = HttpClients.custom()
+//                    .setProxy(new HttpHost("127.0.0.1", 8888))
+                    .setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36")
+                    .setDefaultCookieStore(cookieStore)
+                    .setConnectionManager(new PoolingHttpClientConnectionManager(socketFactoryRegistry))
+                    .build()
+                    .execute(new HttpGet(originalUrl))
+                    .getEntity().getContent();
+            FileUtils.copyInputStreamToFile(is, new File(BASE_PATH + "tencent-original.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     /**
      * 如何判定找到目标滑块位置
      * y轴上至少找到一条长度为30px的白线
      * @throws IOException
      */
     public static int calcMoveDistance() throws IOException {
-        BufferedImage fullBI = ImageIO.read(new File(BASE_PATH + "original.png"));
+        BufferedImage fullBI = ImageIO.read(new File(BASE_PATH + "tencent-original.png"));
         for(int w = 22; w < fullBI.getWidth() - 18; w++){
             int whiteLineLen = 0;
             for (int h = 128; h < fullBI.getHeight() -200; h++){
@@ -85,87 +163,6 @@ public class TencentCrawler {
 
         }
         throw new RuntimeException("计算缺口位置失败");
-    }
-    public static void crawl(){
-        try {
-            driver = new ChromeDriver();
-            driver.manage().window().setSize(new Dimension(1024, 768));
-            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-            driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-            driver.get("https://007.qq.com/online.html?ADTAG=capt.slide");
-            System.out.println(driver.getPageSource());
-            WebElement element = driver.findElement(By.cssSelector("a[data-type='1']"));
-            element.click();
-            Thread.sleep(2 * 1000);
-            driver.findElement(By.id("code")).click();
-            Actions actions = new Actions(driver);
-            driver.switchTo().frame("tcaptcha_popup");
-            String originalUrl = Jsoup.parse(driver.getPageSource()).select("[id=slideBkg]").first().attr("src");
-            System.out.println(originalUrl);
-            downloadOriginalImg(originalUrl, driver.manage().getCookies());
-            int distance = calcMoveDistance();
-            List<MoveEntity> list = getMoveEntity(distance);
-            element = driver.findElement(By.id("tcaptcha_drag_button"));
-            actions.clickAndHold(element).perform();
-            int d = 0;
-
-            for(MoveEntity moveEntity : list){
-                actions.moveByOffset(moveEntity.getX(), moveEntity.getY()).perform();
-                System.out.println("向右总共移动了:" + (d = d + moveEntity.getX()));
-                Thread.sleep(moveEntity.getSleepTime());
-            }
-            actions.release(element).perform();
-            Thread.sleep(2 * 1000);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            driver.quit();
-        }
-    }
-    private static void downloadOriginalImg(String originalUrl, Set<Cookie> cookieSet) {
-        CookieStore cookieStore = new BasicCookieStore();
-        cookieSet.forEach( c -> {
-            BasicClientCookie cookie = new BasicClientCookie(c.getName(), c.getValue());
-            cookie.setPath(c.getPath());
-            cookie.setDomain(c.getDomain());
-            cookie.setExpiryDate(c.getExpiry());
-            cookie.setSecure(true);
-            cookieStore.addCookie(cookie);
-        });
-        InputStream is = null;
-        try {
-            SSLContext sslContext = SSLContexts.custom()
-                    .loadTrustMaterial(KeyStore.getInstance(KeyStore.getDefaultType())
-                            , (chain, authType) -> true).build();
-            Registry<ConnectionSocketFactory> socketFactoryRegistry =
-                    RegistryBuilder.<ConnectionSocketFactory>create()
-                            .register("http", PlainConnectionSocketFactory.INSTANCE)
-                            .register("https", new SSLConnectionSocketFactory(sslContext))
-                            .build();
-            is = HttpClients.custom()
-                    .setProxy(new HttpHost("127.0.0.1", 8888))
-                    .setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36")
-                    .setDefaultCookieStore(cookieStore)
-                    .setConnectionManager(new PoolingHttpClientConnectionManager(socketFactoryRegistry))
-                    .build()
-                    .execute(new HttpGet(originalUrl))
-                    .getEntity().getContent();
-            FileUtils.copyInputStreamToFile(is, new File("/Users/wangyang/Documents/original.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (KeyStoreException e) {
-            e.printStackTrace();
-        } catch (KeyManagementException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
     }
     public static List<MoveEntity> getMoveEntity(int distance){
         List<MoveEntity> list = new ArrayList<>();
